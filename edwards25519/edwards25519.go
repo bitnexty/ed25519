7c7158313d75def1f494084282b4a2e1608f93d1
guotie <guotie@blocklinker.com>
2018-10-25 14:50:11 +0800

    Add function: 1. ScReduce32 2. GeScalarMult 3. GeMul8 4. CachedGroupElementCMove

--- edwards25519/edwards25519.go
+++ edwards25519/edwards25519.go
@@ -715,6 +715,7 @@ func (p *ExtendedGroupElement) ToBytes(s *[32]byte) {
 	s[31] ^= FeIsNegative(&x) << 7
 }
 
+// this is equal monero ge_frombytes_vartime
 func (p *ExtendedGroupElement) FromBytes(s *[32]byte) bool {
 	var u, v, v3, vxx, check FieldElement
 
@@ -778,6 +779,30 @@ func (p *PreComputedGroupElement) Zero() {
 	FeOne(&p.yPlusX)
 	FeOne(&p.yMinusX)
 	FeZero(&p.xy2d)
+}
+
+func (c *CachedGroupElement) Zero() {
+	FeOne(&c.yPlusX)
+	FeOne(&c.yMinusX)
+	FeOne(&c.Z)
+	FeZero(&c.T2d)
+}
+
+func CachedGroupElementCMove(t, u *CachedGroupElement, b int32) {
+	FeCMove(&t.yPlusX, &u.yPlusX, b)
+	FeCMove(&t.yMinusX, &u.yMinusX, b)
+	FeCMove(&t.Z, &u.Z, b)
+	FeCMove(&t.T2d, &u.T2d, b)
+}
+
+// r = 8 * t
+func GeMul8(r *CompletedGroupElement, t *ProjectiveGroupElement) {
+	var u ProjectiveGroupElement
+	t.Double(r)
+	r.ToProjective(&u)
+	u.Double(r)
+	r.ToProjective(&u)
+	u.Double(r)
 }
 
 func geAdd(r *CompletedGroupElement, p *ExtendedGroupElement, q *CachedGroupElement) {
@@ -1790,4 +1815,237 @@ func ScMinimal(scalar *[32]byte) bool {
 	}
 
 	return true
+}
+
+
+func ScReduce32(s *[32]byte) {
+	s0 := 2097151 & load3(s[:])
+	s1 := 2097151 & (load4(s[2:]) >> 5)
+	s2 := 2097151 & (load3(s[5:]) >> 2)
+	s3 := 2097151 & (load4(s[7:]) >> 7)
+	s4 := 2097151 & (load4(s[10:]) >> 4)
+	s5 := 2097151 & (load3(s[13:]) >> 1)
+	s6 := 2097151 & (load4(s[15:]) >> 6)
+	s7 := 2097151 & (load3(s[18:]) >> 3)
+	s8 := 2097151 & load3(s[21:])
+	s9 := 2097151 & (load4(s[23:]) >> 5)
+	s10 := 2097151 & (load3(s[26:]) >> 2)
+	s11 := (load4(s[28:]) >> 7)
+	s12 := int64(0)
+	var carry [12]int64
+	carry[0] = (s0 + (1 << 20)) >> 21
+	s1 += carry[0]
+	s0 -= carry[0] << 21
+	carry[2] = (s2 + (1 << 20)) >> 21
+	s3 += carry[2]
+	s2 -= carry[2] << 21
+	carry[4] = (s4 + (1 << 20)) >> 21
+	s5 += carry[4]
+	s4 -= carry[4] << 21
+	carry[6] = (s6 + (1 << 20)) >> 21
+	s7 += carry[6]
+	s6 -= carry[6] << 21
+	carry[8] = (s8 + (1 << 20)) >> 21
+	s9 += carry[8]
+	s8 -= carry[8] << 21
+	carry[10] = (s10 + (1 << 20)) >> 21
+	s11 += carry[10]
+	s10 -= carry[10] << 21
+	carry[1] = (s1 + (1 << 20)) >> 21
+	s2 += carry[1]
+	s1 -= carry[1] << 21
+	carry[3] = (s3 + (1 << 20)) >> 21
+	s4 += carry[3]
+	s3 -= carry[3] << 21
+	carry[5] = (s5 + (1 << 20)) >> 21
+	s6 += carry[5]
+	s5 -= carry[5] << 21
+	carry[7] = (s7 + (1 << 20)) >> 21
+	s8 += carry[7]
+	s7 -= carry[7] << 21
+	carry[9] = (s9 + (1 << 20)) >> 21
+	s10 += carry[9]
+	s9 -= carry[9] << 21
+	carry[11] = (s11 + (1 << 20)) >> 21
+	s12 += carry[11]
+	s11 -= carry[11] << 21
+
+	s0 += s12 * 666643
+	s1 += s12 * 470296
+	s2 += s12 * 654183
+	s3 -= s12 * 997805
+	s4 += s12 * 136657
+	s5 -= s12 * 683901
+	s12 = 0
+
+	carry[0] = s0 >> 21
+	s1 += carry[0]
+	s0 -= carry[0] << 21
+	carry[1] = s1 >> 21
+	s2 += carry[1]
+	s1 -= carry[1] << 21
+	carry[2] = s2 >> 21
+	s3 += carry[2]
+	s2 -= carry[2] << 21
+	carry[3] = s3 >> 21
+	s4 += carry[3]
+	s3 -= carry[3] << 21
+	carry[4] = s4 >> 21
+	s5 += carry[4]
+	s4 -= carry[4] << 21
+	carry[5] = s5 >> 21
+	s6 += carry[5]
+	s5 -= carry[5] << 21
+	carry[6] = s6 >> 21
+	s7 += carry[6]
+	s6 -= carry[6] << 21
+	carry[7] = s7 >> 21
+	s8 += carry[7]
+	s7 -= carry[7] << 21
+	carry[8] = s8 >> 21
+	s9 += carry[8]
+	s8 -= carry[8] << 21
+	carry[9] = s9 >> 21
+	s10 += carry[9]
+	s9 -= carry[9] << 21
+	carry[10] = s10 >> 21
+	s11 += carry[10]
+	s10 -= carry[10] << 21
+	carry[11] = s11 >> 21
+	s12 += carry[11]
+	s11 -= carry[11] << 21
+
+	s0 += s12 * 666643
+	s1 += s12 * 470296
+	s2 += s12 * 654183
+	s3 -= s12 * 997805
+	s4 += s12 * 136657
+	s5 -= s12 * 683901
+
+	carry[0] = s0 >> 21
+	s1 += carry[0]
+	s0 -= carry[0] << 21
+	carry[1] = s1 >> 21
+	s2 += carry[1]
+	s1 -= carry[1] << 21
+	carry[2] = s2 >> 21
+	s3 += carry[2]
+	s2 -= carry[2] << 21
+	carry[3] = s3 >> 21
+	s4 += carry[3]
+	s3 -= carry[3] << 21
+	carry[4] = s4 >> 21
+	s5 += carry[4]
+	s4 -= carry[4] << 21
+	carry[5] = s5 >> 21
+	s6 += carry[5]
+	s5 -= carry[5] << 21
+	carry[6] = s6 >> 21
+	s7 += carry[6]
+	s6 -= carry[6] << 21
+	carry[7] = s7 >> 21
+	s8 += carry[7]
+	s7 -= carry[7] << 21
+	carry[8] = s8 >> 21
+	s9 += carry[8]
+	s8 -= carry[8] << 21
+	carry[9] = s9 >> 21
+	s10 += carry[9]
+	s9 -= carry[9] << 21
+	carry[10] = s10 >> 21
+	s11 += carry[10]
+	s10 -= carry[10] << 21
+
+	s[0] = byte(s0 >> 0)
+	s[1] = byte(s0 >> 8)
+	s[2] = byte((s0 >> 16) | (s1 << 5))
+	s[3] = byte(s1 >> 3)
+	s[4] = byte(s1 >> 11)
+	s[5] = byte((s1 >> 19) | (s2 << 2))
+	s[6] = byte(s2 >> 6)
+	s[7] = byte((s2 >> 14) | (s3 << 7))
+	s[8] = byte(s3 >> 1)
+	s[9] = byte(s3 >> 9)
+	s[10] = byte((s3 >> 17) | (s4 << 4))
+	s[11] = byte(s4 >> 4)
+	s[12] = byte(s4 >> 12)
+	s[13] = byte((s4 >> 20) | (s5 << 1))
+	s[14] = byte(s5 >> 7)
+	s[15] = byte((s5 >> 15) | (s6 << 6))
+	s[16] = byte(s6 >> 2)
+	s[17] = byte(s6 >> 10)
+	s[18] = byte((s6 >> 18) | (s7 << 3))
+	s[19] = byte(s7 >> 5)
+	s[20] = byte(s7 >> 13)
+	s[21] = byte(s8 >> 0)
+	s[22] = byte(s8 >> 8)
+	s[23] = byte((s8 >> 16) | (s9 << 5))
+	s[24] = byte(s9 >> 3)
+	s[25] = byte(s9 >> 11)
+	s[26] = byte((s9 >> 19) | (s10 << 2))
+	s[27] = byte(s10 >> 6)
+	s[28] = byte((s10 >> 14) | (s11 << 7))
+	s[29] = byte(s11 >> 1)
+	s[30] = byte(s11 >> 9)
+	s[31] = byte(s11 >> 17)
+}
+
+
+// GeScalarMult computes h = a*A, where
+//   a = a[0]+256*a[1]+...+256^31 a[31]
+//   A is a point on the curve
+//
+// Preconditions:
+//   a[31] <= 127
+func GeScalarMult(r *ProjectiveGroupElement, a *[32]byte, A *ExtendedGroupElement) {
+	var e [64]int32
+	var carry, carry2 int32
+	for i := 0; i < 31; i++ {
+		carry += int32(a[i])             /* 0..256 */
+		carry2 = (carry + 8) >> 4        /* 0..16 */
+		e[2*i] = carry - (carry2 << 4)   /* -8..7 */
+		carry = (carry2 + 8) >> 4        /* 0..1 */
+		e[2*i+1] = carry2 - (carry << 4) /* -8..7 */
+	}
+	carry += int32(a[31])         /* 0..128 */
+	carry2 = (carry + 8) >> 4     /* 0..8 */
+	e[62] = carry - (carry2 << 4) /* -8..7 */
+	e[63] = carry2                /* 0..8 */
+
+	var Ai [8]CachedGroupElement // A,2A,3A,4A,5A,6A,7A,8A
+	t := new(CompletedGroupElement)
+	u := new(ExtendedGroupElement)
+	A.ToCached(&Ai[0])
+	for i := 0; i < 7; i++ {
+		geAdd(t, A, &Ai[i])
+		t.ToExtended(u)
+		u.ToCached(&Ai[i+1])
+	}
+	r.Zero()
+	cur := new(CachedGroupElement)
+	minusCur := new(CachedGroupElement)
+	for i := 63; i >= 0; i-- {
+		b := e[i]
+		bNegative := negative(b)
+		bAbs := b - (((-bNegative) & b) << 1)
+		r.Double(t)
+		t.ToProjective(r)
+		r.Double(t)
+		t.ToProjective(r)
+		r.Double(t)
+		t.ToProjective(r)
+		r.Double(t)
+		t.ToExtended(u)
+		cur.Zero()
+		for j := int32(0); j < 8; j++ {
+			CachedGroupElementCMove(cur, &Ai[j], equal(bAbs, j+1))
+		}
+		FeCopy(&minusCur.yPlusX, &cur.yMinusX)
+		FeCopy(&minusCur.yMinusX, &cur.yPlusX)
+		FeCopy(&minusCur.Z, &cur.Z)
+		FeNeg(&minusCur.T2d, &cur.T2d)
+		CachedGroupElementCMove(cur, minusCur, bNegative)
+		geAdd(t, u, cur)
+		t.ToProjective(r)
+	}
 }
